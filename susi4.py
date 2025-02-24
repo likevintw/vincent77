9998655a6905e9786f4a97cce01260eb6b630e21
kengwei.li <likevintw@gmail.com>
2025-02-24 13:13:17 +0800

    update format

--- susi4.py
+++ susi4.py
@@ -4,14 +4,14 @@ import sys
 
 
 class Susi4:
-    def __init__(self,susi4_library_path="./libSUSI-4.00.so"):
-        self.susi4_library=ctypes.CDLL(susi4_library_path)
+    def __init__(self, susi4_library_path="./libSUSI-4.00.so"):
+        self.susi4_library = ctypes.CDLL(susi4_library_path)
         self.initialization()
-        self.SUSIDEMO_INFORMATION_STRING_MAX=9
-        self.gpio_bank_base=int(0x00010000)
-        self.SUSI_ID_GPIO_INPUT_SUPPORT=int(0x00000000)
-        self.SUSI_ID_GPIO_OUTPUT_SUPPORT=int(0x00000001)
-        self.gpio_bank_base=int(0x00010000)
+        self.SUSIDEMO_INFORMATION_STRING_MAX = 9
+        self.gpio_bank_base = int(0x00010000)
+        self.SUSI_ID_GPIO_INPUT_SUPPORT = int(0x00000000)
+        self.SUSI_ID_GPIO_OUTPUT_SUPPORT = int(0x00000001)
+        self.gpio_bank_base = int(0x00010000)
 
     def __del__(self):
         self.susi4_library.SusiLibUninitialize()
@@ -19,24 +19,25 @@ class Susi4:
     def initialization(self):
         self.susi4_library.SusiLibInitialize.restype = ctypes.c_uint32
 
-        status=self.susi4_library.SusiLibInitialize()
-        if status==int(0xFFFFF0FF):
+        status = self.susi4_library.SusiLibInitialize()
+        if status == int(0xFFFFF0FF):
             print("Your Linux capabilities is not enough, recommond to become ROOT!")
             print("Aborting demo now.")
             exit(0)
-        elif status!=0 and status!=int(0xFFFFF0FF):
+        elif status != 0 and status != int(0xFFFFF0FF):
             print("SusiLibInitialize() failed. (0x%08X)", status)
             print("Exit the program...")
             exit(0)
-        print("susi4 initialize successfully: ",status)
+        print("susi4 initialize successfully: ", status)
 
     def get_gpio_information(self):
         # todo, when _RISC system, SUSIDEMO_BANK_MAX=40, esle =4
-        SUSIDEMO_BANK_MAX=40
+        SUSIDEMO_BANK_MAX = 40
         # SUSIDEMO_BANK_MAX=4
-        result=0
+        result = 0
         for pin in range(SUSIDEMO_BANK_MAX):
-            id_number=self.gpio_bank_base+pin
-            state=self.susi4_library.SusiGPIOGetCaps(id_number,self.SUSI_ID_GPIO_INPUT_SUPPORT,result)
-            print("result: ",result)
-            print("state: ",state)
+            id_number = self.gpio_bank_base+pin
+            state = self.susi4_library.SusiGPIOGetCaps(
+                id_number, self.SUSI_ID_GPIO_INPUT_SUPPORT, result)
+            print("result: ", result)
+            print("state: ", state)
--- test_susiiot.py
+++ test_susiiot.py
@@ -5,25 +5,25 @@ import susiiot
 
 class TestHandler(unittest.TestCase):
     def test_get_susi_information(self):
-        handler=susiiot.SusiIot()
-        results=handler.get_susi_information()
+        handler = susiiot.SusiIot()
+        results = handler.get_susi_information()
         print(results)
         print(handler.get_id_list())
 
     def test_get_id_list(self):
-        handler=susiiot.SusiIot()
-        results=handler.get_susi_information()
+        handler = susiiot.SusiIot()
+        results = handler.get_susi_information()
         print(handler.get_id_list())
-    
+
     def test_get_susi_json_t(self):
-        handler=susiiot.SusiIot()
-        results=handler.get_susi_information()
+        handler = susiiot.SusiIot()
+        results = handler.get_susi_information()
         print(handler.get_susi_json_t())
-    
+
     @unittest.skip("todo")
     def test_get_data_by_id(self):
-        handler=susiiot.SusiIot()
-        identities=handler.get_id_list()
+        handler = susiiot.SusiIot()
+        identities = handler.get_id_list()
         print(identities)
         # for identity in identities:
         #     print(handler.get_data_by_id(identity))
@@ -31,9 +31,10 @@ class TestHandler(unittest.TestCase):
 
     @unittest.skip("todo")
     def test_get_data_by_uri(self):
-        handler=susiiot.SusiIot()
-        result=handler.get_data_by_uri("SUSIIoT Information")
+        handler = susiiot.SusiIot()
+        result = handler.get_data_by_uri("SUSIIoT Information")
         print(result)
+
 
 if __name__ == '__main__':
     unittest.main()
