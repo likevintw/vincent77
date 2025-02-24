36831bd3fd3c9be969c80cd47a15f6cde1c4db92
likevintw <likevintw@gmail.com>
2025-02-24 16:16:09 +0800

    add docker run

---
+++ docker_run.sh
@@ -0,0 +1,11 @@
+docker run &&\    
+    -dit &&\    
+    --name susi_demo &&\    
+    --privileged &&\
+    -p 1080:5000 &&\    
+    -p 1081:5001 &&\    
+    -v /sys/:/sys/ &&\    
+    -v /etc/:/etc/     
+    -v /dev/:/dev/ &&\    
+    -v /home/:/volume &&\    
+    ubuntu:20.04
