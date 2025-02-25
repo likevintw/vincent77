599394cee7a6bfbf57a5409b11f28f4102be33c6
likevintw <likevintw@gmail.com>
2025-02-25 11:54:52 +0800

    set todo list

--- run_single_test.sh
+++ run_single_test.sh
@@ -8,3 +8,4 @@ sudo python3 -m unittest -v test_susiiot.TestHandler.test_check_root_authorizati
 # Todo
 sudo python3 -m unittest -v test_susi4.TestHandler.test_get_gpio_information
 sudo python3 -m unittest -v test_susiiot.TestHandler.test_get_data_by_uri
+sudo python3 -m unittest -v test_susiiot.TestHandler.test_get_data_by_id
