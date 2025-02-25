421b423acd2f84b1c3184cf167d5825568128334
likevintw <likevintw@gmail.com>
2025-02-25 13:55:24 +0800

    add test_get_data_by_uri, ok

--- run_single_test.sh
+++ run_single_test.sh
@@ -4,8 +4,8 @@
 # Pass
 sudo python3 -m unittest -v test_susiiot.TestHandler.test_get_susi_information
 sudo python3 -m unittest -v test_susiiot.TestHandler.test_check_root_authorization
+sudo python3 -m unittest -v test_susiiot.TestHandler.test_get_data_by_id
+sudo python3 -m unittest -v test_susiiot.TestHandler.test_get_data_by_uri
 
 # Todo
 sudo python3 -m unittest -v test_susi4.TestHandler.test_get_gpio_information
-sudo python3 -m unittest -v test_susiiot.TestHandler.test_get_data_by_uri
-sudo python3 -m unittest -v test_susiiot.TestHandler.test_get_data_by_id
