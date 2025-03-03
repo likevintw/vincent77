df2e15faa209e24c359167f621ab54877951b8b4
likevintw <likevintw@gmail.com>
2025-03-03 14:04:16 +0800

    update docker file

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ FROM ubuntu:20.04
 ENV DEBIAN_FRONTEND=noninteractive
 
 RUN apt-get update && \
-    apt-get install -y python3 python3-pip libjansson4
+    apt-get install -y python3 python3-pip libjansson4&& \
+    rm -rf /var/lib/apt/lists/*
--- docker_run.sh
+++ docker_run.sh
@@ -11,3 +11,18 @@ docker run \
     -v /home/:/volume \
     susiiot_ubuntu:1.0.1 \
     /bin/bash
+
+docker run \
+    --rm \
+    -it \
+    --name susi_iot_demo \
+    --privileged \
+    -v /sys/:/sys/ \
+    -v /proc/:/proc/ \
+    -v /etc/:/etc/ \
+    -v /dev/:/dev/ \
+    -v /var/:/var/ \
+    -v /lib/:/lib/ \
+    -v /home/:/volume \
+    susiiot_ubuntu:1.0.1 \
+    /bin/bash
