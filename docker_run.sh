7683cca425200c8758f8e72ffdff7e6ed7828dd4
kengwei <likevintw@gmail.com>
2025-03-03 03:10:44 +0000

    update docker run

--- docker_run.sh
+++ docker_run.sh
@@ -3,7 +3,10 @@ docker run \
     --name susi_iot_demo \
     --privileged \
     -v /sys/:/sys/ \
+    -v /proc/:/proc/ \
     -v /etc/:/etc/ \
     -v /dev/:/dev/ \
+    -v /var/:/var/ \
+    -v /lib/:/lib/ \
     -v /home/:/volume \
     susiiot_ubuntu:1.0.1
