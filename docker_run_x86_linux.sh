8c6ec31c1e9239dd0e80c4615a5283b2d69eae10
likevintw <likevintw@gmail.com>
2025-03-03 15:25:16 +0800

    less information

--- docker_run_x86_linux.sh
+++ docker_run_x86_linux.sh
@@ -1,12 +1,9 @@
 docker run \
+    --rm \
     -it \
     --name susi_iot_demo \
     --privileged \
-    -v /sys/:/sys/ \
-    -v /proc/:/proc/ \
-    -v /etc/:/etc/ \
-    -v /dev/:/dev/ \
-    -v /var/:/var/ \
+    -v /sys/firmware/efi/efivars/:/sys/firmware/efi/efivars/ \
     -v /home/:/volume \
     susiiot_ubuntu:1.0.1 \
     bash
