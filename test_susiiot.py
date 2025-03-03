589ce36cf7b66c3392698d128d3bd78f57df5564
kengwei <likevintw@gmail.com>
2025-03-03 02:09:58 +0000

    remove comment

--- test_susiiot.py
+++ test_susiiot.py
@@ -9,36 +9,35 @@ class TestHandler(unittest.TestCase):
     def test_get_susi_information(self):
         handler = susiiot.SusiIot()
         results = handler.get_susi_information()
-        # print(results)
-        # print(handler.get_id_list())
+        print(results)
 
     def test_get_id_list(self):
         handler = susiiot.SusiIot()
         results = handler.get_id_list()
-        # print(results)
+        print(results)
 
     def test_get_data_by_id(self):
         handler = susiiot.SusiIot()
         id_list = handler.get_id_list()
-        # for device_id in id_list:
-        #     print(handler.get_data_by_id(device_id))
+        for device_id in id_list:
+            print(handler.get_data_by_id(device_id))
 
     def test_get_data_by_uri(self):
         handler = susiiot.SusiIot()
         uris=handler.susi_information.keys()
-        # for uri in uris:
-        #     result = handler.get_data_by_uri(uri)
-        #     print(result)
+        for uri in uris:
+            result = handler.get_data_by_uri(uri)
+            print(result)
 
     def test_check_root_authorization(self):
         handler = susiiot.SusiIot()
         results = handler.check_root_authorization()
-        # print(results)
+        print(results)
 
     def test_get_log_path(self):
         handler = susiiot.SusiIot()
         results = handler.get_log_path()
-        # print(results)
+        print(results)
 
     def test_get_json_format_data(self):
         handler = susiiot.SusiIot()
@@ -47,28 +46,28 @@ class TestHandler(unittest.TestCase):
         results = handler.get_json_format_data(0.0)
         self.assertEqual(results.type, 4)
         results = handler.get_json_format_data("")
-        # print(results.type)
+        print(results.type)
 
     def test_get_system_temperature(self):
         handler = susiiot.SusiIot()
         result = handler.get_system_temperature_in_celsius()
-        # print(result)
+        print(result)
 
     def test_get_id_dictionary(self):
         handler = susiiot.SusiIot()
         results = handler.get_id_dictionary()
-        # for key in results.keys():
-        #     print(key)
+        for key in results.keys():
+            print(key)
 
     def test_set_gpio_00(self):
         handler = susiiot.SusiIot()
         if 17039873 in handler.id_list:
             result = handler.set_value(17039873, 0)
             result = handler.get_data_by_id(17039873)
-            # print(result)
+            print(result)
             result = handler.set_value(17039873, 1)
             result = handler.get_data_by_id(17039873)
-            # print(result)
+            print(result)
 
 
 if __name__ == '__main__':
