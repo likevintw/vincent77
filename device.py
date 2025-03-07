c4a51913e2dab3a62126fb1d9e1225e5404bc6d5
likevintw <likevintw@gmail.com>
2025-03-07 11:58:56 +0800

    update format

--- device.py
+++ device.py
@@ -2,14 +2,15 @@
 import susiiot
 
 
-
 class Device:
     def __init__(self):
-        self.susiiot=susiiot.SusiIot()
+        self.susiiot = susiiot.SusiIot()
+
     @property
     def cpu_temperature(self):
         return self.susiiot.get_cpu_tempature()
 
-device=Device()
+
+device = Device()
 device.cpu_temperature
-device.cpu_temperature=5
+device.cpu_temperature = 5
--- platformsdk.py
+++ platformsdk.py
@@ -1,18 +1,20 @@
 
 
-EAPI_ID_BOARD_MANUFACTURER_STR					=0
-EAPI_ID_BOARD_NAME_STR						=1
-EAPI_ID_BOARD_REVISION_STR					=2
-EAPI_ID_BOARD_SERIAL_STR					=3
-EAPI_ID_BOARD_BIOS_REVISION_STR					=4
-EAPI_ID_BOARD_HW_REVISION_STR					=5
-EAPI_ID_BOARD_PLATFORM_TYPE_STR					=6
+
+
 
 class PlatformSDK:
     def __init__(self):
-        self.e_api_library=None
-        self.EAPI_ID_BOARD_MANUFACTURER_STR=0
+        self.e_api_library = None
         
+        self.EAPI_ID_BOARD_MANUFACTURER_STR = 0
+        self.EAPI_ID_BOARD_NAME_STR = 1
+        self.EAPI_ID_BOARD_REVISION_STR = 2
+        self.EAPI_ID_BOARD_SERIAL_STR = 3
+        self.EAPI_ID_BOARD_BIOS_REVISION_STR = 4
+        self.EAPI_ID_BOARD_HW_REVISION_STR = 5
+        self.EAPI_ID_BOARD_PLATFORM_TYPE_STR = 6
+
         self.import_library()
         self.initialize()
 
@@ -20,7 +22,7 @@ class PlatformSDK:
         current_dir = os.path.dirname(os.path.realpath(__file__))+"/"
         architecture = platform.machine()
         os_name = platform.system()
-        e_api_library_path=""
+        e_api_library_path = ""
 
         if os_name == "Linux" and 'x86' in architecture.lower():
             e_api_library_path = current_dir+"libEAPI.x86.so"
@@ -38,6 +40,6 @@ class PlatformSDK:
             print(f"disable to import library, architechture:{architecture.lower()}, os:{os_name}")
 
         self.e_api_library = ctypes.CDLL(e_api_library_path)
-        
+
     def initialize(self):
-        self.e_api_library=None
+        self.e_api_library = None
