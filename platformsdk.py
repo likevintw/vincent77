23150c885652b3b5a8d35ccd20ad1a113780b899
kengwei.li <likevintw@gmail.com>
2025-03-10 15:18:05 +0800

    update EApiGetMemoryAvailable

--- platformsdk.py
+++ platformsdk.py
@@ -175,5 +175,5 @@ class PlatformSDK:
 
     def get_available_memory(self):
         available_memory=0
-        status=self.EApiGetMemoryAvailable(available_memory)
+        status=self.EApiGetMemoryAvailable(ctypes.byref(available_memory))
         print(status,available_memory)
