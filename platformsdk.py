450bec989361575a8b00ebb2a8d89b8fc9197a0b
kengwei.li <likevintw@gmail.com>
2025-03-10 11:43:45 +0800

    add error message

--- platformsdk.py
+++ platformsdk.py
@@ -223,7 +223,11 @@ class PlatformSDK:
         n=int(n)
         if n < 0:
             n = (1 << 32) + n
-        return hex(n)
+        n=hex(n)
+        if n=="0xFFFFF0FF":
+            return "EAPI_STATUS_ERROR"
+        elif n=="0xFFFFFCFF":
+            return "EAPI_STATUS_UNSUPPORTED"
     
     def get_board_string_data(self, id_number):
         # 緩衝區大小和初始化
