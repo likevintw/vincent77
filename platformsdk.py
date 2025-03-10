862a67284e72622781f842af84de263605db2fea
kengwei.li <likevintw@gmail.com>
2025-03-10 11:23:29 +0800

    add handle_error_code

--- platformsdk.py
+++ platformsdk.py
@@ -219,6 +219,10 @@ class PlatformSDK:
         self.EApiLibInitialize = prototype(
             ("EApiGPIOGetLevel", self.e_api_library))
         
+    def handle_error_code(self,n):
+        if n < 0:
+            n = (1 << 32) + n
+        return hex(n)
     def get_board_string_data(self, id_number):
         # 緩衝區大小和初始化
         CMD_RETURN_BUF_SIZE = 4096
--- test_platformsdk.py
+++ test_platformsdk.py
@@ -19,31 +19,39 @@ class TestHandler(unittest.TestCase):
         print()
         for i in range(10):
             status = handler.get_board_string_data(i)
+            status=handler.handle_error_code(status)
             print(i, status)
         for i in range(16, 19):
             status = handler.get_board_string_data(i)
+            status=handler.handle_error_code(status)
             print(i, status)
         for i in range(257, 260):
             status = handler.get_board_string_data(i)
+            status=handler.handle_error_code(status)
             print(i, status)
         for i in range(513, 526):
             status = handler.get_board_string_data(i)
+            status=handler.handle_error_code(status)
             print(i, status)
         for i in range(327680, 327690):
             status = handler.get_board_string_data(i)
+            status=handler.handle_error_code(status)
             print(i, status)
         for i in range(330240, 330265):
             status = handler.get_board_string_data(i)
+            status=handler.handle_error_code(status)
             print(i, status)
 
     def test_get_board_value_data(self):
         handler = platformsdk.PlatformSDK()
         status = handler.get_board_value_data(handler.EAPI_ID_HWMON_TEMP_CPU)
+        status=handler.handle_error_code(status)
         print(status)
 
     def test_initial_EApiLibrary(self):
         handler = platformsdk.PlatformSDK()
         status = handler.initial_EApiLibrary()
+        status=handler.handle_error_code(status)
         print(status)
 
 
