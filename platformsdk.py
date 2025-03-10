4e24c7790540d3cb7d4f13dbe3131af512ffed20
kengwei.li <likevintw@gmail.com>
2025-03-10 14:19:07 +0800

    fix error

--- platformsdk.py
+++ platformsdk.py
@@ -72,13 +72,13 @@ class PlatformSDK:
             "EAPI_ID_HWMON_FAN_CPU": 0x00052000,
             "EAPI_ID_HWMON_FAN_SYSTEM": 0x00052001,
         }
-        for i in self.board_information_string.keys():
-            print(i,self.board_information_string[i])
-            result=self.get_board_string_data(self.board_information_string[i])
-            self.board_information.update({i:result})
         for i in self.board_information_value.keys():
             print(i,self.board_information_value[i])
             result=self.get_board_string_data(self.board_information_value[i])
+            self.board_information.update({i:result})
+        for i in self.board_information_string.keys():
+            print(i,self.board_information_string[i])
+            result=self.get_board_string_data(self.board_information_string[i])
             self.board_information.update({i:result})
         print(self.board_information)
 
