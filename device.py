2af52154127f5534cef9091b1c191e86431d67cb
likevintw <likevintw@gmail.com>
2025-03-19 11:04:05 +0800

    remove useless code

--- device.py
+++ device.py
@@ -4,27 +4,10 @@ import susiiot
 
 class Device:
     def __init__(self):
-        self.cusor = None
-
-        self.boot_up_time = None
-        self.running_time_in_hours = None
-        self.board_name = None
-        self.board_manufacturer = None
-        self.bios_version = None
-        self.firmware_name = None
-        self.firmware_version = None
-        self.driver_version = None
-        self.library_version = None
-        self.voltage_vcore = None
-        self.voltage_5v_standby = None
-        self.voltage_cmos_battery = None
-        self.voltage_dc = None
-        self.cpu_temperature_in_celsius = None
-        self.system_temperature_in_celsius = None
-        self.cpu_fan_speed = None
-        self.system_fan_speed = None
+        self.library = None
 
     def initialize():
+        # check device version to determine library, susi, platform or others
         pass
 
     @property
