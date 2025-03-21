5d060ce9462692c875396ea1d7d237362160c410
likevintw <likevintw@gmail.com>
2025-03-21 15:41:30 +0800

    before remove -v

--- docker_run.sh
+++ docker_run.sh
@@ -25,3 +25,23 @@ sudo docker run \
     -v /home/:/volume \
     susiiot_ubuntu:1.0.1 \
     /bin/bash
+
+sudo docker run \
+    -it \
+    --name susi_iot_demo \
+    --privileged \
+    -v /sys/:/sys/ \
+    -v /proc/:/proc/ \
+    -v /etc/:/etc/ \
+    -v /dev/:/dev/ \
+    -v /var/:/var/ \
+    -v /home/:/volume \
+    ubuntu:20.04 \
+    /bin/bash
+
+apt-get update && \
+    apt-get install -y git git-core python3 python3-pip libjansson4 
+
+git clone https://github.com/likevintw/vincent77.git && \
+    cd vincent77 && \
+    sudo python3 -m unittest -v test_susiiot.TestCases > "$(date +\%Y\%m\%d\%H\%M\%S)_result.txt"
