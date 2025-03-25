d8824101e856a0660b0cb9da897e797383e5d736
kengwei.li <likevintw@gmail.com>
2025-03-25 10:00:17 +0800

    remove interface

--- susiiot.py
+++ susiiot.py
@@ -790,54 +790,6 @@ class SusiIot:
         except:
             return None
 
-    
-    @property
-    @abstractmethod
-    def name(self) -> str:
-        pass
-
-    @property
-    @abstractmethod
-    def cpu_model(self) -> str:
-        pass
-
-    @property
-    @abstractmethod
-    def os_revision(self) -> str:
-        pass
-
-    @property
-    @abstractmethod
-    def bios_revision(self) -> str:
-        try:
-            id_number = self.susi_id_name_table["BIOS revision"]
-            return self.get_data_by_id(id_number)["sv"]
-        except:
-            return None
-
-    @property
-    @abstractmethod
-    def ec_revision(self) -> str:
-        pass
-
-    @abstractmethod
-    def get_voltage(self, voltage_source) -> float:
-        pass
-
-    @abstractmethod
-    def get_temperature(self, temperature_source) -> float:
-        pass
-
-    @property
-    @abstractmethod
-    def voltage_sources(self) -> List[str]:
-        pass
-
-    @property
-    @abstractmethod
-    def temperature_sources(self) -> List[str]:
-        pass
-
 class JsonType:
     JSON_OBJECT = 0
     JSON_ARRAY = 1
