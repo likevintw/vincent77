2cc0b6f64418dd1e0c16163df162262a497dc88a
kengwei.li <likevintw@gmail.com>
2025-03-26 11:36:49 +0800

    add R result

--- docker_run.sh
+++ docker_run.sh
@@ -1,41 +1,41 @@
-docker run \
-    -dit \
-    --name susi_iot_demo \
-    --privileged \
-    -v /sys/:/sys/ \
-    -v /proc/:/proc/ \
-    -v /etc/:/etc/ \
-    -v /dev/:/dev/ \
-    -v /var/:/var/ \
-    -v /lib/:/lib/ \
-    -v /home/:/volume \
-    susiiot_ubuntu:1.0.1 \
-    /bin/bash
+# docker run \
+#     -dit \
+#     --name susi_iot_demo \
+#     --privileged \
+#     -v /sys/:/sys/ \
+#     -v /proc/:/proc/ \
+#     -v /etc/:/etc/ \
+#     -v /dev/:/dev/ \
+#     -v /var/:/var/ \
+#     -v /lib/:/lib/ \
+#     -v /home/:/volume \
+#     susiiot_ubuntu:1.0.1 \
+#     /bin/bash
 
-sudo docker run \
-    --rm \
-    -it \
-    --name susi_iot_demo \
-    --privileged \
-    -v /sys/:/sys/ \
-    -v /proc/:/proc/ \
-    -v /etc/:/etc/ \
-    -v /dev/:/dev/ \
-    -v /var/:/var/ \
-    -v /home/:/volume \
-    susiiot_ubuntu:1.0.1 \
-    /bin/bash
+# sudo docker run \
+#     --rm \
+#     -it \
+#     --name susi_iot_demo \
+#     --privileged \
+#     -v /sys/:/sys/ \
+#     -v /proc/:/proc/ \
+#     -v /etc/:/etc/ \
+#     -v /dev/:/dev/ \
+#     -v /var/:/var/ \
+#     -v /home/:/volume \
+#     susiiot_ubuntu:1.0.1 \
+#     /bin/bash
 
-sudo docker run \
-    -it \
-    --rm \
-    --name susi_iot_demo \
-    --privileged \
-    -v /sys/firmware/efi/efivars/:/sys/firmware/efi/efivars/ \
-    -v /etc/:/etc/ \
-    -v /home/:/volume \
-    ubuntu:20.04 \
-    /bin/bash
+# sudo docker run \
+#     -it \
+#     --rm \
+#     --name susi_iot_demo \
+#     --privileged \
+#     -v /sys/firmware/efi/efivars/:/sys/firmware/efi/efivars/ \
+#     -v /etc/:/etc/ \
+#     -v /home/:/volume \
+#     ubuntu:20.04 \
+#     /bin/bash
 
 sudo docker run \
     -it \
